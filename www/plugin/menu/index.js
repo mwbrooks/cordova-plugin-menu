2e507a13522b5d849a61ad53c4a2fa3e9062bdc3
Michael Brooks <michael@michaelbrooks.ca>
2011-05-10 17:02:15 -0700

    Refactor JavaScript

--- www/plugin/menu/index.js
+++ www/plugin/menu/index.js
@@ -1,41 +1,91 @@
 (function(window) {
 
-    //
-    // HTMLMenuElement
-    //
+    // generate a unique ID
+    var uuid = function() { return ++this.nextId; };
+    uuid.nextId = 0;
 
+    /**
+     * HTMLMenuElement
+     * ===============
+     *
+     * Creates a new HTML Menu Element with a unique ID.
+     *
+     * The unique ID is stored as the attribute 'data-uuid'
+     *
+     * Since the type of menu is not yet defined, there is nothing to render.
+     */
     window.HTMLMenuElement = function() {
-        var self = this;
-        var attr = {};
-
-        // Unused at the moment
-        ['label', 'type'].forEach(function(property) {
-            Object.defineProperty(self, property, {
-                get: function()  { return attr[property]; },
-                set: function(v) { attr[property] = v; }
-            });
-        });
-
-        var success = function() { console.log('created toolbar'); };
-        var fail    = function() { console.log('failed to create toolbar'); };
-        PhoneGap.exec(success, fail, 'ca.michaelbrooks.menu.toolbar', 'new');
+        this.attribute = {
+            'data-uuid': uuid()
+        };
     };
 
-    HTMLMenuElement.prototype.setAttribute = function(name, value) {
-        this[name] = value;
-
-        var success = function() { console.log('setAttribute success => ' + name + ': ' + value); };
-        var fail    = function() { console.log('setAttribute failure => ' + name + ': ' + value); };
-
-        PhoneGap.exec(success, fail, 'ca.michaelbrooks.menu.toolbar', name, [value]);
-    };
-
+    /**
+     * getAttribute
+     * ------------
+     *
+     * Return an attribute with the given name.
+     *
+     *     menu.getAttribute('data-uuid');
+     */
     HTMLMenuElement.prototype.getAttribute = function(name) {
-        return this[name];
+        return this.attribute[name];
     };
-    
+
+    /**
+     * hasAttribute
+     * ------------
+     *
+     * Return a boolean for whether the attribute exists.
+     *
+     * An attribute exists if it has a value other than undefined.
+     *
+     *     menu.hasAttribute('data-uuid'); // true
+     */
     HTMLMenuElement.prototype.hasAttribute = function(name) {
-        return (typeof this[name] !== 'undefined');
+        return (typeof this.attribute[name] !== 'undefined');
+    };
+
+    /**
+     * setAttribute
+     * ------------
+     *
+     * Sets the element's attribute and invokes any actions that
+     * are consequences of the attribute.
+     *
+     * ### Type
+     *
+     *     menu.setAttribute('type', 'toolbar'); // create a toolbar
+     *     menu.setAttribute('type', 'context'); // create a context menu
+     *
+     * ### Label
+     *
+     *     menu.setAttribute('label', 'My Title'); // give menu a title
+     */
+    HTMLMenuElement.prototype.setAttribute = function(name, value) {
+        this.attribute[name] = value;
+
+        var id = this.attribute['data-uuid'];
+
+        switch(name) {
+            case 'type':
+                // destroy existing menu
+                // create new menu
+                // apply attributes
+                PhoneGap.exec(
+                    function(){},
+                    function(){},
+                    'ca.michaelbrooks.menu.toolbar', 'type', [id, value]
+                );
+                break;
+            case 'label':
+                PhoneGap.exec(
+                    function(){},
+                    function(){},
+                    'ca.michaelbrooks.menu.toolbar', 'label', [id, value]
+                );
+                break;
+        };
     };
 
     HTMLMenuElement.prototype.appendChild = function(element) {
@@ -44,11 +94,10 @@
     HTMLMenuElement.prototype.removeChild = function(element) {
     };
 
-    //
-    // Override document.createElement
-    //
-
+    // backup original `document.createElement`
     var _createElement = document.createElement;
+
+    // override `document.createElement` to support menu
     document.createElement = function() {
         if (arguments[0] === 'menu')
             return new HTMLMenuElement();
