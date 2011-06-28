98819b11bb7296aa4c186840bc7853d7fdfc88e9
Michael Brooks <michael@michaelbrooks.ca>
2011-06-28 14:14:30 -0700

    Update Android comments.

--- native/android/com/phonegap/menu/AppMenu.java
+++ native/android/com/phonegap/menu/AppMenu.java
@@ -18,6 +18,11 @@ public class AppMenu extends Plugin {
 	private ArrayList <MenuInfo> menuItems;
 	private boolean menuChanged;
 	
+	/**
+	 * PhoneGap Exec
+	 *
+	 * Handle a incoming call from JavaScript.
+	 */
 	@Override
 	public PluginResult execute(String action, JSONArray args, String callbackId) {
 		if(action.equals("create")) {
@@ -37,59 +42,35 @@ public class AppMenu extends Plugin {
 		return r;
 	}
 	
-	public void addMenuItem(MenuInfo info) {
-		if (menuItems == null) {
-			menuItems = new ArrayList<MenuInfo>();
-		}
-		
-		menuItems.add(info);
-		this.updateMenu();
-		
-		if (android.os.Build.VERSION.RELEASE.startsWith("3.")) {
-			buildHoneycombMenu(ctx.dMenu);
-		}
+	/**
+	 * Plugin onDestroy
+	 *
+	 * Detach static singleton when application is destroyed.
+	 */
+	@Override
+	public void onDestroy() {
+		AppMenu.singleton = null;
 	}
 	
-	public void removeMenuItem(MenuInfo menuItem) {
-		int index = menuItems.indexOf(menuItem);
-		menuItems.remove(index);
-		this.updateMenu();
-	}
-	
-	public MenuInfo getMenuItem(int id) {
-		MenuInfo menuItem = null;
-		ListIterator<MenuInfo> iter = menuItems.listIterator();
-		
-		while (iter.hasNext()) {
-			MenuInfo tmpMenuItem = iter.next();
-			
-			if (tmpMenuItem.id == id) {
-				menuItem = tmpMenuItem;
-				break;
-			}
-		}
-		
-		return menuItem;
-	}
-	
-	public void updateMenu() {
-		menuChanged = true;
-	}
-	
-	private void createMenu(JSONArray args) {
-		if (AppMenu.singleton == null) {
-			AppMenu.singleton = this;
-		}
-		
-		if (menuItems == null) {
-			menuItems = new ArrayList<MenuInfo>();
-		}
-	}
-	
+	/**
+	 * Menu Changed Check
+	 *
+	 * DroidGap checks if the menu must be re-rendered
+	 */
+	@Override
 	public boolean isMenuChanged() {
 		return menuChanged;
 	}
 	
+	/**
+	 * Render the menu.
+	 *
+	 * This is called when the menu is first created
+	 * and when the menu needs to be updated. Unfortunately,
+	 * DroidGap clears the menu before calling this function,
+	 * so the menu must be rendered from scratch.
+	 */
+	@Override
 	public boolean buildMenu(Menu menu) {
 		ListIterator<MenuInfo> iter = menuItems.listIterator();
 		
@@ -107,6 +88,90 @@ public class AppMenu extends Plugin {
 		return true;
 	}
 	
+	/**
+	 * Menu Item Selected
+	 *
+	 * Called when a given menu item has been selected.
+	 */
+	public boolean onMenuItemSelected(MenuItem item) {
+		webView.loadUrl("javascript:window.HTMLCommandElement.elements['" + item.getItemId() + "'].attribute.action();");
+		return true;
+	}
+	
+	/**
+	 * Create the Android Menu.
+	 */
+	private void createMenu(JSONArray args) {
+		if (AppMenu.singleton == null) {
+			AppMenu.singleton = this;
+		}
+		
+		if (menuItems == null) {
+			menuItems = new ArrayList<MenuInfo>();
+		}
+	}
+	
+	/**
+	 * Add Item to Menu
+	 *
+	 * Inserts a menu item into the menu. It does not alter
+	 * the menu item.
+	 */
+	public void addMenuItem(MenuInfo info) {
+		if (menuItems == null) {
+			menuItems = new ArrayList<MenuInfo>();
+		}
+		
+		menuItems.add(info);
+		this.updateMenu();
+		
+		if (android.os.Build.VERSION.RELEASE.startsWith("3.")) {
+			buildHoneycombMenu(ctx.dMenu);
+		}
+	}
+	
+	/**
+	 * Remove Item from Menu
+	 *
+	 * Removes a referenced item from the menu.
+	 */
+	public void removeMenuItem(MenuInfo menuItem) {
+		int index = menuItems.indexOf(menuItem);
+		menuItems.remove(index);
+		this.updateMenu();
+	}
+	
+	/**
+	 * Get Menu Item
+	 *
+	 * Retrieves a menu item based on the ID.
+	 */
+	public MenuInfo getMenuItem(int id) {
+		MenuInfo menuItem = null;
+		ListIterator<MenuInfo> iter = menuItems.listIterator();
+		
+		while (iter.hasNext()) {
+			MenuInfo tmpMenuItem = iter.next();
+			
+			if (tmpMenuItem.id == id) {
+				menuItem = tmpMenuItem;
+				break;
+			}
+		}
+		
+		return menuItem;
+	}
+	
+	/**
+	 * Flag the menu to be updated.
+	 */
+	public void updateMenu() {
+		menuChanged = true;
+	}
+	
+	/**
+	 * Honeycomb Menu Support
+	 */
 	public boolean buildHoneycombMenu(final Menu menu) {
 		final AppMenu that = this;
 		
@@ -120,14 +185,5 @@ public class AppMenu extends Plugin {
 		menuChanged = false;
 		
 		return true;
-	}
-	
-	public boolean onMenuItemSelected(MenuItem item) {
-		webView.loadUrl("javascript:window.HTMLCommandElement.elements['" + item.getItemId() + "'].attribute.action();");
-		return true;
-	}
-	
-	public void onDestroy() {
-		AppMenu.singleton = null;
 	}
 }
