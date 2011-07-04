e38f66df8ae7944d901f310cc532d50fa22075c0
Michael Brooks <michael@michaelbrooks.ca>
2011-07-04 14:01:23 -0700

    Load Android toolbar menu immediately.

--- www/plugin/menu/index.js
+++ www/plugin/menu/index.js
@@ -397,25 +397,25 @@
 //
 
 if (navigator.userAgent.match(/android/i) && typeof window.PhoneGap !== 'undefined') {
+    PhoneGap.originalExec = PhoneGap.exec;
+
+    PhoneGap.exec = function(success, fail, service, action, args) {
+        if (service === 'com.phonegap.menu.toolbar') {
+            window.toolbar[action](success, fail, args);
+        }
+        else if (service === 'com.phonegap.menu.toolbar.command') {
+            try {
+            window.toolbarCommand[action](success, fail, args);
+            } catch(e) { console.log(e); }
+        }
+        else {
+            PhoneGap.originalExec(success, fail, service, action, args);
+        }
+    };
+    
     PhoneGap.addConstructor(function() {
         navigator.app.addService('com.phonegap.menu.context',         'com.phonegap.menu.AppMenu');
         navigator.app.addService('com.phonegap.menu.context.command', 'com.phonegap.menu.AppMenuItem');
-
-        PhoneGap.originalExec = PhoneGap.exec;
-
-        PhoneGap.exec = function(success, fail, service, action, args) {
-            if (service === 'com.phonegap.menu.toolbar') {
-                window.toolbar[action](success, fail, args);
-            }
-            else if (service === 'com.phonegap.menu.toolbar.command') {
-                try {
-                window.toolbarCommand[action](success, fail, args);
-                } catch(e) { console.log(e); }
-            }
-            else {
-                PhoneGap.originalExec(success, fail, service, action, args);
-            }
-        };
     });
 }
 
