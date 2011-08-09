cbf61759154a6979f883f0e96ec0f3cc2ffdcfee
Michael Brooks <michael@michaelbrooks.ca>
2011-08-09 14:10:54 -0700

    Connect Android's basic JavaScript functionality.

--- www/plugin/menu/android.js
+++ www/plugin/menu/android.js
@@ -18,21 +18,4 @@ if (navigator.userAgent.match(/android/i) && typeof window.PhoneGap !== 'undefin
             PhoneGap.originalExec(success, fail, service, action, args);
         }
     };
-    
-    PhoneGap.addConstructor(function() {
-        navigator.app.addService('com.phonegap.menu.context',         'com.phonegap.menu.AppMenu');
-        navigator.app.addService('com.phonegap.menu.context.command', 'com.phonegap.menu.AppMenuItem');
-        
-        var version = navigator.device.version.split('.');
-        version.forEach(function(value, index) {
-            version[index] = parseInt(value);
-        });
-        
-        Help.menuPosition = (version[0] > 2 || (version[0] >= 2 && version[1] >= 2)) ? 'fixed' : 'absolute';
-        
-        var toolbar = document.getElementById('phonegap-menu-toolbar');
-        if (toolbar) {
-            toolbar.style.position = Help.menuPosition;
-        }
-    });
 }
