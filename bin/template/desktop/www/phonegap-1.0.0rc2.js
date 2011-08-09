1c45f831d6bc5b0b13561ad1981ea26409cc7f3a
Michael Brooks <michael@michaelbrooks.ca>
2011-08-09 16:16:33 -0700

    Move targets into bin directory.

---
+++ bin/template/desktop/www/phonegap-1.0.0rc2.js
@@ -0,0 +1,22 @@
+(function(window) {
+    //
+    // Stub PhoneGap
+    //
+
+    window.PhoneGap = {
+        exec: function(success, fail, service, action, args) {
+            console.log('Ignored: PhoneGap.exec(success, fail, ' + service + ', ' + action + ', ' + JSON.stringify(args) + ');');
+            fail();
+        }
+    };
+
+    window.addEventListener('load', function() {
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent('deviceready', true, false);
+
+        setTimeout(function() {
+            document.dispatchEvent(event);
+        }, 300);
+    }, false);
+
+})(window);
