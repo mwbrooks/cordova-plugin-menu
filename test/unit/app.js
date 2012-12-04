30fdabc6737f239048ed984c677b48d345029094
Michael Brooks <michael@michaelbrooks.ca>
2012-12-04 12:07:38 -0800

    Remove outdated project.

--- test/unit/app.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-
-    var menu = document.createElement('menu');
-    menu.setAttribute('type',  'toolbar');
-    menu.setAttribute('label', 'HTMLMenuElement Demo');
-
-})(window);
